6b824718314c63553cbae12153a64812e2e57efa
Mike Splain <msplain@paypal.com>
2014-06-20 00:06:07 -0400

    Version bump to 0.1.1 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mike.splain@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures openstack-clients'
 long_description 'Installs/Configures openstack-clients'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'python'
 depends 'apt'
